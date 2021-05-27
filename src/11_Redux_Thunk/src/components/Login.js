dfb8474f797d192bc81c0075d81613674ded6186
rianbtk <mrprobojonegoro@gmail.com>
2021-05-28 03:31:39 +0700

    11

---
+++ src/11_Redux_Thunk/src/components/Login.js
@@ -0,0 +1,71 @@
+import { Link } from "react-router-dom";
+import routes from "./routes";
+import { useState } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { loginUser } from "../redux/actions/auth";
+
+function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleForm = e => {
+    e.preventDefault();
+
+    const { dispatch } = props;
+    dispatch(loginUser(email, password));
+  };
+
+  const { isLoggingIn, loginError, isAuthenticated } = props;
+
+  if (isAuthenticated) {
+    return <Redirect to="/" />
+  } else {
+    return (
+      <div>
+        <ul className="nav">
+          {routes.map((route, i) => (
+            <li key={i}>
+              <Link to={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <h1>Login</h1>
+        <form onSubmit={e => handleForm(e)}>
+          <input
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            name="email"
+            type="email"
+            placeholder="email"
+            required
+          />
+          <input
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            name="password"
+            type="password"
+            placeholder="password"
+            required
+          />
+          <hr />
+          <button type="submit">Login</button>
+          <hr />
+          <span>{loginError && ("Email atau Password Salah!")}</span>
+          <span>{isLoggingIn && ("Sedang login ...")}</span>
+        </form>
+      </div>
+    );
+  }
+
+}
+
+function mapStateToProps(state) {
+  return {
+    isLoggingIn: state.auth.isLoggingIn,
+    loginError: state.auth.loginError,
+    isAuthenticated: state.auth.isAuthenticated
+  }
+}
+
+export default connect(mapStateToProps)(Login);
---
+++ src/11_Redux_Thunk/src/firebase.config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJPS8PrAKjt88sMCjk7PVG7HOXNidiYZg",
+  authDomain: "login-form03.firebaseapp.com",
+  projectId: "login-form03",
+  storageBucket: "login-form03.appspot.com",
+  messagingSenderId: "793931456411",
+  appId: "1:793931456411:web:92703710d4b8e7c9864b3e",
+  measurementId: "G-8RXGE25X7Q"
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
