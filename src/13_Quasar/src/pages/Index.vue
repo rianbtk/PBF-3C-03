feb1503a80ddc0dcdf0127f042c3bc237ef970d9
rianbtk <mrprobojonegoro@gmail.com>
2021-06-20 13:54:01 +0700

    src

---
+++ src/13_Quasar/src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>{{ hello }}</h3>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      hello: "Halo, Nama Saya Agus Salim Hadjrianto dengan NIM 1941723007"
+    }
+  }
+}
+</script>
